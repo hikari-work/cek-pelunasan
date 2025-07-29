af04081d9ac183d97399865d3caa8c6c996e7041
Viandra Stefani <viandrastefani754@gmail.com>
2025-07-29 20:06:08 +0700

    Update DTO, Async In Controller

--- src/main/java/org/cekpelunasan/controller/WebhookController.java
+++ src/main/java/org/cekpelunasan/controller/WebhookController.java
@@ -1,5 +1,8 @@
 package org.cekpelunasan.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cekpelunasan.bot.TelegramBot;
 import org.cekpelunasan.dto.WhatsappMessageDTO;
 import org.cekpelunasan.service.whatsapp.WhatsappRouters;
@@ -14,6 +17,8 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 public class WebhookController {
 
@@ -21,11 +26,14 @@ public class WebhookController {
 	private final TelegramClient telegramClient;
 	private final TelegramBot telegramBot;
 	private final WhatsappRouters whatsappRouters;
+	private final ObjectMapper objectMapper;
 
-	public WebhookController(@Value("${telegram.bot.token}") String botToken, TelegramBot telegramBot, WhatsappRouters whatsappRouters1) {
+	public WebhookController(@Value("${telegram.bot.token}") String botToken, TelegramBot telegramBot, WhatsappRouters whatsappRouters1, ObjectMapper objectMapper) {
 		this.telegramClient = new OkHttpTelegramClient(botToken);
 		this.telegramBot = telegramBot;
 		this.whatsappRouters = whatsappRouters1;
+		this.objectMapper = objectMapper;
+
 	}
 
 	@PostMapping("/webhook")
@@ -34,9 +42,19 @@ public class WebhookController {
 		return "Webhook is working!";
 	}
 	@PostMapping("/whatsapp")
-	public ResponseEntity<String> whatsapp(@RequestBody WhatsappMessageDTO whatsappMessageDTO) {
-		log.info("Updated {}", whatsappMessageDTO);
-		whatsappRouters.sendPelunasanOrTabungan(whatsappMessageDTO);
+	public ResponseEntity<String> whatsapp(@RequestBody JsonNode whatsappMessage) {
+		JsonNode eventNode = whatsappMessage.get("event");
+		if (eventNode != null) {
+			return ResponseEntity.ok("OK");
+		}
+		log.info("Received {}", whatsappMessage);
+		WhatsappMessageDTO whatsappMessageDTO;
+		try {
+			whatsappMessageDTO = objectMapper.treeToValue(whatsappMessage, WhatsappMessageDTO.class);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+		CompletableFuture.runAsync(() -> whatsappRouters.sendPelunasanOrTabungan(whatsappMessageDTO));
 		return ResponseEntity.ok("OK");
 	}
 }
