92b982df9d73f3fdd919ac0d302894ebb326f3bd
Viandra Stefani <viandrastefani754@gmail.com>
2025-08-01 21:45:50 +0700

    Not Yet Fix Repayment Calculator in Webview

--- src/main/java/org/cekpelunasan/controller/RepaymentController.java
+++ src/main/java/org/cekpelunasan/controller/RepaymentController.java
@@ -1,4 +1,68 @@
 package org.cekpelunasan.controller;
 
+
+import org.cekpelunasan.entity.Repayment;
+import org.cekpelunasan.service.repayment.RepaymentService;
+import org.cekpelunasan.utils.PenaltyUtils;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class RepaymentController {
+
+	private static final Logger log = LoggerFactory.getLogger(RepaymentController.class);
+	private final RepaymentService repaymentService;
+	private final PenaltyUtils penaltyUtils;
+
+	public RepaymentController(RepaymentService repaymentService, PenaltyUtils penaltyUtils) {
+		this.repaymentService = repaymentService;
+		this.penaltyUtils = penaltyUtils;
+	}
+
+	@GetMapping("/pelunasan")
+	public String showWebApp(@RequestParam(required = false) String initData, Model model) {
+		model.addAttribute("initData", initData);
+		model.addAttribute("repayments", repaymentService.findAllLimited());
+		log.info("initData: {}", initData);
+		return "webapp";
+	}
+
+	@GetMapping("/api/search")
+	@ResponseBody
+	public List<Repayment> searchRepayments(@RequestParam String searchTerm) {
+		if (searchTerm == null || searchTerm.trim().isEmpty()) {
+			log.info("Searching for all repayments...");
+			List<Repayment> allLimited = repaymentService.findAllLimited();
+			return getRepayments(allLimited);
+		}
+		List<Repayment> repayments = repaymentService.searchByIdOrNameLimited(searchTerm.trim());
+		return getRepayments(repayments);
+	}
+
+	@NotNull
+	private List<Repayment> getRepayments(List<Repayment> allLimited) {
+		allLimited.forEach(r -> {
+			Map<String, Long> penalty = penaltyUtils.penalty(r.getStartDate(), r.getAmount(), r.getProduct(), r);
+			r.setPenaltyRepayment(penalty.get("penalty"));
+			log.info("Penalty: {}", penalty.get("penalty"));
+			log.info("Multiplier : {}", penalty.get("multiplier"));
+		});
+		return allLimited;
+	}
+
+	@GetMapping("/tabungan")
+	public String tabungan() {
+		return "OK";
+	}
+	
+	@GetMapping("/simulasi")
+	public String simulasi() {
+		return "OK";
+	}
 }
--- src/main/java/org/cekpelunasan/utils/button/HelpButton.java
+++ src/main/java/org/cekpelunasan/utils/button/HelpButton.java
@@ -1,6 +1,8 @@
 package org.cekpelunasan.utils.button;
 
 import org.cekpelunasan.service.NgrokService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @Component
 public class HelpButton {
+	private static final Logger log = LoggerFactory.getLogger(HelpButton.class);
 	private final NgrokService ngrokService;
 
 	public HelpButton(NgrokService ngrokService) {
@@ -24,7 +27,7 @@ public InlineKeyboardMarkup sendHelpMessage() {
 
 	InlineKeyboardButton repaymentButton = InlineKeyboardButton.builder()
 		.text("📸 Tampilan Pelunasan")
-		.webApp(new WebAppInfo(ngrokUrl+ "/pelunasan"))
+		.webApp(new WebAppInfo(ngrokUrl + "/pelunasan"))
 		.build();
 	InlineKeyboardButton billsButton = InlineKeyboardButton.builder()
 		.text("📸 Tampilan Tagihan")
@@ -38,7 +41,7 @@ public InlineKeyboardMarkup sendHelpMessage() {
 		.text("📸 Tampilan Kolek Tas")
 		.webApp(new WebAppInfo(ngrokUrl+ "/kolektas"))
 		.build();
-	
+	log.info("Created Url {}", repaymentButton.getWebApp().getUrl());
 	rows.add(new InlineKeyboardRow(repaymentButton));
 	rows.add(new InlineKeyboardRow(billsButton));
 	rows.add(new InlineKeyboardRow(savingsButton));
