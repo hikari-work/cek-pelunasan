15014cec5294463c1c3ea751ec6399ce12210fa8
piyan <vianstefani754@gmail.com>
2025-09-22 07:47:25 +0700

    Different Method Of Selecting Branch

--- src/main/java/org/cekpelunasan/repository/BillsRepository.java
+++ src/main/java/org/cekpelunasan/repository/BillsRepository.java
@@ -35,6 +35,15 @@ public interface BillsRepository extends JpaRepository<Bills, String> {
 		""")
 	Page<Bills> findByMinInterestOrMinPrincipalIsGreaterThanAndBranch(Long minInterest, Long minPrincipal, String branch, Pageable pageable);
 
+
+
+	@Query("""
+		SELECT b FROM Bills b
+		WHERE b.kios = :branch
+		AND (b.minInterest + b.minPrincipal) > 0
+		""")
+	Page<Bills> findByMinInterestOrMinPrincipalIsGreaterThanAndBranchLocation(Long minInterest, Long minPrincipal, String branch, Pageable pageable);
+
 	@Query("""
 		SELECT b FROM Bills b
 		WHERE b.accountOfficer = :accountOfficer
@@ -42,6 +51,13 @@ public interface BillsRepository extends JpaRepository<Bills, String> {
 		""")
 	Page<Bills> findByMinInterestOrMinPrincipalIsGreaterThanAndAccountOfficer(Long minInterest, Long minPrincipal, String accountOfficer, Pageable pageable);
 
+
+	@Query("""
+		SELECT b FROM Bills b
+		WHERE b.branch = :branch AND b.kios = ''
+		AND (b.minInterest + b.minPrincipal) > 0
+		""")
+	Page<Bills> findMinimalPaymentForInduk(Long minInterest, Long minPrincipal, String branch, Pageable pageable);
 
 	@Query("SELECT DISTINCT b.branch FROM Bills b")
 	Set<String> findDistinctBranchByBrach();
--- src/main/java/org/cekpelunasan/service/Bill/HotKolekService.java
+++ src/main/java/org/cekpelunasan/service/Bill/HotKolekService.java
@@ -7,6 +7,7 @@ import org.cekpelunasan.repository.BillsRepository;
 import org.cekpelunasan.repository.PayingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,12 +42,28 @@ public class HotKolekService {
         return filterBills(branch, 
             (repo, br) -> repo.findByDueDateContaining(getMonth()));
     }
+	public List<Bills> findDueDateByKios(String branch, String kios) {
+		return filterBills(branch,
+			(repo, br) -> repo.findByDueDateContaining(getMonth()));
+	}
     
     public List<Bills> findFirstPay(String branch) {
 		log.info("Finding {}", getLastMonth());
         return filterBills(branch, 
             (repo, br) -> repo.findByRealizationIsContainingIgnoreCase(getLastMonth()));
     }
+	public List<Bills> findFirstPayByKios(String branch, String kios) {
+		log.info("Finding {}", getLastMonth());
+		return filterBills(branch,
+			(repo, br) -> repo.findByRealizationIsContainingIgnoreCase(getLastMonth()));
+	}
+
+
+	public List<Bills> findFirstPayByLocation(String branch) {
+		log.info("Finding {}", getLastMonth());
+		return filterBillsByOffice(branch,
+			(repo, br) -> repo.findByRealizationIsContainingIgnoreCase(getLastMonth()));
+	}
     
     public List<Bills> findMinimalPay(String branch) {
 		List<Bills> bills = filterBills(branch,
@@ -54,6 +71,21 @@ public class HotKolekService {
 				0L, 0L, br, Pageable.unpaged()).stream().toList()));
 		bills.removeIf(bill -> bill.getAccountOfficer().equals("JKS"));
 		return bills;
+	}
+
+	public List<Bills> findMinimalPayment(String branch) {
+		List<Bills> bills = filterBillsByOffice(branch,
+			(repo, br) -> new ArrayList<>(repo.findByMinInterestOrMinPrincipalIsGreaterThanAndBranchLocation(
+				0L, 0L, br, Pageable.unpaged()).stream().toList()));
+		bills.removeIf(bill -> bill.getAccountOfficer().equals("JKS"));
+		return bills;
+	}
+
+	public List<Bills> findMinimalPayForInduk(String branch) {
+		Page<Bills> minimalPaymentForInduk = billsRepository.findMinimalPaymentForInduk(0L, 0L, branch, Pageable.unpaged());
+		List<Bills> content = minimalPaymentForInduk.getContent();
+		content.removeIf(bill -> bill.getAccountOfficer().equals("JKS"));
+		return content;
 	}
     
     private List<Bills> filterBills(String branch, BiFunction<BillsRepository, String, List<Bills>> billsFetcher) {
@@ -63,6 +95,14 @@ public class HotKolekService {
         bills.removeIf(bill -> !bill.getOfficeLocation().equals(branch));
         return bills;
     }
+
+	private List<Bills> filterBillsByOffice(String branch, BiFunction<BillsRepository, String, List<Bills>> billsFetcher) {
+		List<String> payings = payingRepository.findAll().stream().map(Paying::getId).toList();
+		List<Bills> bills = billsFetcher.apply(billsRepository, branch);
+		bills.removeIf(bill -> payings.contains(bill.getNoSpk()));
+		bills.removeIf(bill -> !bill.getKios().equals(branch));
+		return bills;
+	}
 
 	@Transactional
 	public void saveAllPaying(List<String> list) {
